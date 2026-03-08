b50a75af9d84107f8f7662b8137c9c7aedf74564
touhidhasan8 <touhidhasan1200@gmail.com>
2026-03-08 18:46:23 +0600

    added a Login Functionalitis

---
+++ Js File/script.js
@@ -0,0 +1,16 @@
+ document
+        .getElementById("login-btn")
+        .addEventListener("click", function () {
+          const number = document.getElementById("input-number");
+          const inputNumber = number.value;
+
+          const pin = document.getElementById("pin");
+          const inputPin = pin.value;
+
+          if (inputNumber == "admin" && inputPin == "admin123") {
+            alert("login Successfully ✅ ");
+            window.location.assign('./home.html')
+          } else {
+            alert("invalid Number And Pin Number");
+          }
+        });
