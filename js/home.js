c0198a2c00274271e4e4ecee510b220649a98b82
touhidhasan8 <touhidhasan1200@gmail.com>
2026-03-09 19:47:54 +0600

    data fetching and modal add

--- js/home.js
+++ js/home.js
@@ -20,6 +20,74 @@ const addActive = () => {
   closeBtn.classList.add("active");
 };
 
+// Modal Data Fetching
+const cardDetails = async (id) => {
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`,
+  );
+  const data = await res.json();
+  modalData(data.data);
+};
+
+const modalData = (modal) => {
+  const modalCard = document.getElementById("modals");
+  modalCard.innerHTML = `    
+  <dialog id="cardDetailsModal" class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+   
+  
+  <div class="card-body">
+    <h2 class="card-title text-2xl font-bold">
+      ${modal.title}
+    </h2>
+    <div class="flex items-center gap-5">
+    <div class="p-2 rounded-md text-white bg-green-500">
+  ${modal.status === "open" ? "Opened" : "Closed"}
+</div>
+      
+      <p class="text-[#64748B]">${modal.assignee} </p>
+      <p class="text-[#64748B]">${modal.createdAt}</p>
+    </div>
+  
+    <div class="card-actions flex "> 
+              ${modal.labels
+                .map(
+                  (label) => `
+              <p class="  badge badge-warning ">${label.toUpperCase()}</p>
+            `,
+                )
+                .join("")}
+        </div>
+ 
+
+
+    <p class="text-[#64748B]">${modal.description}</p>
+    <div class="card-actions flex p-5 bg-gray-200 rounded-md justify-between">
+      
+       <div>
+       <p class="text-[#64748B]">Assign</p>
+       <p class="font-bold">${modal.assignee.toUpperCase()}</p>
+       </div>
+       <div>
+       <p>Priority</p>
+       <p class="badge badge-error text-white">${modal.priority.toUpperCase()}</p>
+       
+       </div>
+     
+    </div>
+  </div>
+
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn btn-primary">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>`;
+  document.getElementById("cardDetailsModal").showModal();
+};
+
 let allCardData = [];
 // Data Fetching and Functionalities Adding
 async function allJobFetching() {
@@ -36,8 +104,8 @@ allJobFetching();
 
 const allJobs = (job) => {
   const allBtn = document.getElementById("cardSection");
-  const cardLength = document.getElementById('cardCounter');
-cardLength.textContent = `${job.length}`
+  const cardLength = document.getElementById("cardCounter");
+  cardLength.textContent = `${job.length}`;
   job.forEach((items) => {
     const div = document.createElement("div");
     div.innerHTML = `
@@ -59,7 +127,7 @@ cardLength.textContent = `${job.length}`
         ${items.description}
       </p>
 
-      <div>
+      </div>
         <div class="card-actions flex "> 
               ${items.labels
                 .map(
@@ -83,52 +151,59 @@ cardLength.textContent = `${job.length}`
   });
 };
 
-
 const cardRemove = () => {
   const card = document.getElementById("cardSection");
   card.innerHTML = "";
 };
 
-
 // Reset all buttons
 const removeActive = () => {
-  const allBtn = document.getElementById('allbtn');
-  const openBtn = document.getElementById('openBtn');
-  const closeBtn = document.getElementById('closeBtn');
+  const allBtn = document.getElementById("allbtn");
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
 
   // Remove active
-  allBtn.classList.remove('active');
-  openBtn.classList.remove('active');
-  closeBtn.classList.remove('active');
+  allBtn.classList.remove("active");
+  openBtn.classList.remove("active");
+  closeBtn.classList.remove("active");
 
   // Reset colors
-  allBtn.classList.remove('bg-[#4A00FF]', 'text-white');
-  openBtn.classList.remove('bg-[#4A00FF]', 'text-white');
-  closeBtn.classList.remove('bg-[#4A00FF]', 'text-white');
+  allBtn.classList.remove("bg-[#4A00FF]", "text-white");
+  openBtn.classList.remove("bg-[#4A00FF]", "text-white");
+  closeBtn.classList.remove("bg-[#4A00FF]", "text-white");
 };
 
 // All button
-document.getElementById('allbtn').addEventListener('click', function() {
+document.getElementById("allbtn").addEventListener("click", function () {
   removeActive();
-  this.classList.add('active'); 
+  this.classList.add("active");
   cardRemove();
   allJobs(allCardData);
 });
 
 // Open button
-document.getElementById('openBtn').addEventListener('click', function() {
+document.getElementById("openBtn").addEventListener("click", function () {
   removeActive();
-  this.classList.add('active');  
-  const openData = allCardData.filter(item => item.status=='open');
+  this.classList.add("active");
+  const openData = allCardData.filter((item) => item.status == "open");
   cardRemove();
   allJobs(openData);
 });
 
 // Close button
-document.getElementById('closeBtn').addEventListener('click', function() {
+document.getElementById("closeBtn").addEventListener("click", function () {
   removeActive();
-  this.classList.add('active');  
-  const closeData = allCardData.filter(item => item.status=='closed');
+  this.classList.add("active");
+  const closeData = allCardData.filter((item) => item.status == "closed");
   cardRemove();
   allJobs(closeData);
 });
+
+// Modal
+
+// const cardDetails =async ()=>{
+//   const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues`)
+//     const data = await res.json()
+//     modalData(data.data)
+// }
+// searchIssu()
